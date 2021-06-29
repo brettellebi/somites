f4021418f3adf23caa6f0b4ead0ee8965b73964a
Ian Brettell <brettell@ebi.ac.uk>
2021-06-29 16:26:45 +0100

    20210629 update from local

--- docs/source.R
+++ docs/source.R
@@ -17,3 +17,9 @@ names(pal_hom_het_1) = c(0:2, "UNCLASSIFIED")
 
 pal_hom_het_2 = c("#e28413", "#000022", "#DE3C4B", "#FBF5F3")
 names(pal_hom_het_2) = c(0:2, "UNCLASSIFIED")
+
+pal_hom_het_2_lines = c(karyoploteR::darker(pal_hom_het_2[1], 100),
+                        karyoploteR::lighter(pal_hom_het_2[2], 100),
+                        karyoploteR::darker(pal_hom_het_2[3], 100),
+                        karyoploteR::darker(pal_hom_het_2[4]))
+names(pal_hom_het_2_lines) = c(0:2, "UNCLASSIFIED")
